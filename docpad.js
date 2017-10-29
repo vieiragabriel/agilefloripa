debec74a664b02360bb8b25c3e95f34285efca17
Jonathan Henrique de Souza <jonathan.henrique@icloud.com>
2017-10-29 20:06:38 -0200

    Add KMP I course

--- docpad.js
+++ docpad.js
@@ -72,23 +72,23 @@ module.exports = {
     },
 
     courses: [
-    //   {
-    //     name: "Rodrigo Yoshima",
-    //     photo: "themes/agile-floripa/img/speaker/RodrigoYoshima.jpg",
-    //     company: "",
-    //     bio: "Técnico em Processamento de Dados e Bacharel em Administração de Empresas e Economia. É Advanced Accredited Lean Kanban Trainer (AAKT) e Kanban Coach Professional (KCP) pela Lean-Kanban University. Com mais de 20 anos de experiência na área de tecnologia, Rodrigo atua como consultor e instrutor em melhoria de processos tendo implantado métodos Lean e Agile em mais de 200 equipes em mais de 30 empresas. Casado, pai e velejador.",
-    //     presentation: {
-    //       title: "Treinamento Kanban (KMP I) + Ingresso FREE para o 3º Agile Floripa(condicionado a vagas disponíveis)",
-    //       featured: "\\o/ BIS ",
-    //       description: "Aprenda a melhorar os processos da sua empresa de forma evolucionária, lidando com a resistência a mudanças e ajustando aos objetivos da sua organização. A Aspercom é pioneira no Brasil na aplicação do Método Kanban em pequenas, médias e grandes empresas nos mais variados setores. Com a nossa experiência você aprende neste treinamento como melhorar o seu processo de forma evolucionária - lidando com a resistência natural das pessoas a mudanças - tornando seu processo ajustado aos objetivos de negócio da sua organização! Este treinamento é certificado Lean-Kanban University (Kanban Method Professional I).",
-    //       local: "CIASC",
-    //       date: "16 e 17 de março",
-    //       time: "08:00 às 17:00",
-    //       value: "R$ 900,00",
-		  // cta: 'http://aspercom.com.br/kanban-floripa/',
-		  // ctaText: 'Inscreva-se'
-    //     }
-    //   }
+      {
+        name: "Rodrigo Yoshima",
+        photo: "themes/agile-floripa/img/speaker/RodrigoYoshima.jpg",
+        company: "",
+        bio: "Técnico em Processamento de Dados e Bacharel em Administração de Empresas e Economia. É Advanced Accredited Lean Kanban Trainer (AAKT) e Kanban Coach Professional (KCP) pela Lean-Kanban University. Com mais de 20 anos de experiência na área de tecnologia, Rodrigo atua como consultor e instrutor em melhoria de processos tendo implantado métodos Lean e Agile em mais de 200 equipes em mais de 30 empresas. Casado, pai e velejador.",
+        presentation: {
+          title: "Treinamento Kanban (KMP I) + Ingresso FREE para o 4º Agile Floripa (válido enquanto houverem vagas no evento)",
+          //featured: "\\o/ BIS ",
+          description: "Aprenda a melhorar os processos da sua empresa de forma evolucionária, lidando com a resistência a mudanças e ajustando aos objetivos da sua organização. A Aspercom é pioneira no Brasil na aplicação do Método Kanban em pequenas, médias e grandes empresas nos mais variados setores. Com a nossa experiência você aprende neste treinamento como melhorar o seu processo de forma evolucionária - lidando com a resistência natural das pessoas a mudanças - tornando seu processo ajustado aos objetivos de negócio da sua organização! Este treinamento é certificado Lean-Kanban University (Kanban Method Professional I).",
+          local: "CIASC",
+          date: "7 e 8 de Dezembro",
+          time: "08:00 às 17:00",
+          value: "Early Bird (10 vagas ou até 15/Nov): R$ 1.150 / Normal (10 vagas ou até 5/Dez): R$ 1.350",
+		  cta: 'http://aspercom.com.br/kanban-floripa/',
+		  ctaText: 'Inscreva-se'
+        }
+      }
     ],
 
     // The entire schedule
