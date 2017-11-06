3bfa4634039b25213b1c1ae89999b11a4804e055
Jonathan Henrique de Souza <jonathan.henrique@icloud.com>
2017-11-06 21:41:38 -0200

    New schedule

--- docpad.js
+++ docpad.js
@@ -93,103 +93,140 @@ module.exports = {
 
     // The entire schedule
     schedule: [
-      {
-        name: "Credenciamento e Welcome coffee.",
-        time: "08:30"
-      },
-      {
-         name: "",
-          // photo: "themes/agile-floripa/img/speaker/antonio.jpg",
-          company: "",
-          bio: "",
-          presentation: {
-              title: "",
-              description: "",
-              time: ""
-          }
-      },
-      {
-          name: "",
-          // photo: "themes/agile-floripa/img/speaker/antonio.jpg",
-          company: "",
-          bio: "",
-          presentation: {
-              title: "",
-              description: "",
-              time: ""
-          }
-      },
-      {
-          name: "Coffee break",
-          time: "10:40"
-      },
-      {
-          name: "",
-          // photo: "themes/agile-floripa/img/speaker/antonio.jpg",
-          company: "",
-          bio: "",
-          presentation: {
-              title: "",
-              description: "",
-              time: ""
-          }
-      },
-      {
-          name: "",
-          // photo: "themes/agile-floripa/img/speaker/antonio.jpg",
-          company: "",
-          bio: "",
-          presentation: {
-              title: "",
-              description: "",
-              time: ""
-          }
-      },
-      {
-        name: "Almoço",
-        time: "12:30"
-      },
-      {
-          name: "",
-          // photo: "themes/agile-floripa/img/speaker/antonio.jpg",
-          company: "",
-          bio: "",
-          presentation: {
-              title: "",
-              description: "",
-              time: ""
-          }
-      },
-      {
-          name: "",
-          // photo: "themes/agile-floripa/img/speaker/antonio.jpg",
-          company: "",
-          bio: "",
-          presentation: {
-              title: "",
-              description: "",
-              time: ""
-          }
-      },
-      {
-        name: "Coffee break",
-        time: "15:40"
-      },
-      {
-          name: "",
-          // photo: "themes/agile-floripa/img/speaker/antonio.jpg",
-          company: "",
-          bio: "",
-          presentation: {
-              title: "",
-              description: "",
-              time: ""
-          }
-      },
-      {
-        name: "Encerramento + Sorteios e Happy Hour \\o/",
-        time: "17:10"
-      }
+        {
+            name: "Credenciamento e Welcome coffee.",
+            time: "08:15"
+        },
+        {
+            name: "Abertura",
+            time: "08:45"
+        },
+        {
+            name: "Luiz Cláudio Parzianello",
+            photo: "themes/agile-floripa/img/speaker/parzianello.jpg",
+            company: "",
+            bio: "Consultor, palestrante e professor universitário. Atua na área de TI há mais de 25 anos e de gestão há 15 anos, sendo um dos pioneiros na adoção e disseminação das Metodologias Ágeis no Brasil. Tornou-se especialista em resolução de problemas complexos de negócio utilizando pensamento sistêmico e modelos de análise (Lean Business Analysis, autor) e de gestão (Agile Management). Atuou para empresas como Grupo RBS, Sicredi, Unicred, Banrisul, Citibank, ThyssenKruppElevadores, FAURGS, REFAP/Petrobrás, GetNet, Ministério do Planejamento de Angola, entre outras. Engenheiro Eletricista pela PUCRS e Mestre em Sistemas Eletrônicos pela USP, é sócio-fundadorda empresa Surya Gestão Digital e professor em MBA nas instituições UniRitter, UNISC, IMED e CESUSC. Foi fundador e presidente do IIBA Porto Alegre Chapter (International Institute of Business Analysis) e um dos autores da Agile Extension to the BABOK (Business Analysis Body of Knowledge), projeto conjunto entre IIBA e Agile Alliance.",
+            presentation: {
+                title: "Keynote: Pragamatismo, Agilidade e Resultados na Gestão de Negócios",
+                description: "O pragmatismo sempre esteve no discurso dos profissionais que escreveram e fizeram o Manifesto Ágil se tornar uma realidade mundial. Da mesma forma, acreditamos que o crescimento ou a sobrevivência de negócios no século XXI depende de uma gestão focada em resultados, que vai além da implantação de práticas e ferramentas ágeis. A gestão é responsabilidade de todos e esta palestra irá demonstrar a maior mudança que ainda falta no mundo empresarial.",
+                time: "09:00"
+            }
+        },
+        {
+            name: "Coffee break",
+            time: "10:00"
+        },
+        {
+            name: "Paulo Antiquera",
+            photo: "themes/agile-floripa/img/speaker/user.png",
+            company: "ArcTouch",
+            bio: "Tenho desenvolvido software por quase toda minha vida, profissionalmente a uns 20 anos. Nos últimos 7 anos venho procurando formas mais eficazes de gerar valor aos clientes. Nesta jornada já misturei práticas das mais diversas fontes: Xp, Kanban, Scrum, PMI... Como sou apaixonado por software já fiz de tudo um pouco na área:\n\n* Escrevi muito código (<3): Delphi, Asp, PHP, C, C++, Java, Javascript, Shell...\n* Tive que automatizar tarefas (principalmente builds), cuidar de repositórios de fontes, fazer muito merge, liberar versões de sistema, subir ambientes para teste e produção...\n* Já tive que montar arquitetura de sistemas, backend, frontend, desenhar armazenamento de dados, integração entre sistemas (XML para cá, json para lá, protocolo, socket, tcp, udp...)\n* Certa vez quando olhei para minha mesa tinha uma carta dizendo: parabéns você foi promovido, agora olha para frente, você precisa dar um jeito de estes 40 caras na sua frente deixarem os nossos clientes satisfeitos...\n* Negociei escopo, negociei prazo, negociei orçamento, negociei estratégia, conheci os stakeholders, anotei que eles queriam, fiz aquele diagrama de sequencia que os devs precisavam, escrevi a história épica, quebrei em features, depois em user stories...\n* Conversei bastante, sobre excelência técnica, testes automatizados, ciclos de feedback curtos, sobre pagar os débitos técnicos, sobre autonomia, entrega contínua, fluxo, melhoria contínua...\n\nE o melhor, tudo isto continua fazendo parte da minha vida \o/,  mas não ao mesmo tempo. Agora gosto de fazer uma coisa por vez, a coisa certa e somente a coisa certa!",
+            presentation: {
+                title: "The HEART, Osborn! First, we attack his HEART! Lições e Estratégias Aprendidas em 7 anos de Agilidade",
+                description: "A medida em que métodos vão surgindo para endereçar problemas, cada vez mais de alto nível, perdemos inconscientemente a conexão com a essência, que é resolver os problemas de negócio. Frameworks são importantes, mas o sucesso das práticas passa pela compreensão de todo contexto do negócio e a natureza do trabalho.\n\nDe que agilidade estamos falando\n* Faça a coisa certa e somente a coisa certa.\n* Primeiro a eficácia. A eficiência é pressuposto.\n* Ciclos de feedback curtos.\n* Todas as fichas na coisa certa.\n\nO aprendizado\nAo trabalhar com métodos ágeis desde 2010, houve falhas e bons resultados. Por conta do ambiente (contexto), neste período, foram constatadas uma série de questões e, dentre estas, 10 lições aprendidas:\n\n1. Inicie pelo Mindset\n2. De a devida importância ao contexto\n3. Respeite os estágios de aprendizado\n4. A compreensão vem com a vivência\n5. Saiba que nem toda pipoca estoura\n6. Procure excelência técnica para obter sustentabilidade\n7. Como gestor mantenha o foco no bastão\n8. Seja um organismo e não um mecanismo\n9. Diga o quê, não como\n10. Traga a organização para o jogo.",
+                time: "10:30"
+            }
+        },
+        {
+            name: "Paula Martins & Clóvis Melo",
+            photo: "themes/agile-floripa/img/speaker/user.png",
+            company: "Unicred",
+            bio: "Paula Martins:\n\nHá 15 anos na área de TI, de desenvolvedora, analista de testes, líder até Analista de Governança e Agile coaching. Formada em Gestão em tecnologia da informação, certificação em Coaching Comportamental em TI. Ministrante de treinamentos internos empresarial, participante e palestrante do grupo de usuários de testes de software em Porto Alegre.\n\nClóvis Melo:\n\nExperiência com Governança de TI de 5 anos em Liderança e acompanhamento do Planejamento Estratégico, definição e apuração dos principais indicadores de desempenho da TI. Gerenciamento dos Processos de TI com avaliação de Riscos e acompanhamento do nível de maturidade dos processos com utilização da metodologia COBIT e ITIL.",
+            presentation: {
+                title: "Como a Governança de TI pode acelerar a transformação ágil",
+                description: "Com o novo modelo e mindset de agilidade, existem algumas áreas que devem caminhar junto e apoiar este novo formato de trabalho e a Governança tem papel fundamental para essa transformação, apoiando de forma que os times estabeleçam transparência, confiança e principalmente colaboração. Detentora do alinhamento entre negócios e TI,  a Governança tem informações e controles que podem acelerar este movimento.",
+                time: "11:10"
+            }
+        },
+        {
+            name: "Raphael Tucunduva Gonçalves",
+            photo: "themes/agile-floripa/img/speaker/user.png",
+            company: "CIASC",
+            bio: "",
+            presentation: {
+                title: "Aprimorando a administração pública: Métodos ágeis em prol do príncípio da eficiência na Administração Pública",
+                description: "A palestra pretende apresentar a experiência de adoção de técnicas ágeis por um conjunto de times do Centro de Informática e  Automação do Estado de SC (CIASC). A intenção é que se evidenciem os benefícios que esse esforço propicia para o atendimento ao Princípio Constitucional da Eficiência na Administração Pública, no intento de estimular que práticas análogas se propaguem para outros grupos  voltados a atender necessidades dos cidadãos catarinenses.",
+                time: "11:50"
+            }
+        },
+        {
+            name: "Almoço",
+            time: "12:30"
+        },
+        {
+            name: "Marcelo Leite Barros",
+            photo: "themes/agile-floripa/img/speaker/marceloleite.jpg",
+            company: "Lambda3",
+            bio: "Personal & Professional Coach, Agilista criativo, curioso e detalhista, apaixonado por música, cachorros, tatuagens, batata frita, praia, cultura organizacional e comportamento humano. 20 anos de experiência em TI, ajudando times a entregarem software de qualidade e alto valor de negócio.",
+            presentation: {
+                title: "Popcorn Flow e o Papel do Agile Coach na Resolução de Problemas",
+                description: "Certa vez fui chamado para ajudar um time com problemas... Mais que ajudá-los, procurei ensinar ferramentas que o time pudesse usar para resolver seus desafios, de forma clara e objetiva, sem mimimi. Essa é a grande sacada do trabalho de um Agile Coach: sem estar próximo do dia a dia, dar ferramentas, ensinar, conduzir as pessoas para a melhoria contínua. Entenda como conduzi este processo, utilizando o Popcorn Flow como ferramenta e veja os resultados desta jornada! Esta sessão demonstra um case de como apoiei um time a discutir de forma objetiva seus desafios. Por mais incrível que pareça, o meu maior desafio nessa jornada foi ajudar a definir o que era o problema! Com o pensamento estruturado, livre de mimimi e sendo objetivos, o time chegou a uma quantidade bem considerável de insights para resolver as causas raiz.",
+                time: "14:00"
+            }
+        },
+        {
+            name: "Helal Ferrari Cabral",
+            photo: "themes/agile-floripa/img/speaker/user.png",
+            company: "Taller",
+            bio: "Analista de negócios apaixonado por processos. Através do desenvolvimento emergi para o mundo dos negócios de tecnologia :-), CERTIFIED PPTO.",
+            presentation: {
+                title: "Pare de priorizar e comece a selecionar",
+                description: "Priorização significa que existe uma fila fixa de items que devem ser processados. No contexto do desenvolvimento de software de escopo aberto, não priorizamos um conjunto de demandas, e sim selecionamos as melhores com criterios definidos com o cliente dentro de uma nuvem de idéias. O objetivo dessa palestra é mostrar como criar e usar esses criterios para ter uma melhor seleção das demanas que serão criadas pelo time.",
+                time: "14:40"
+            }
+        },
+        {
+            name: "Allex Espindola Erckmann",
+            photo: "themes/agile-floripa/img/speaker/allexespindola.jpg",
+            company: "Intelbras",
+            bio: "Natural de Florianópolis/SC, graduado em Gestão da Tecnologia da Informação pelo SENAC e Pós Graduado em Gerenciamento de Projetos de TI pela Unisul, é certificado SAFe Practitioner e SAFe Agilist pela Scaled Agile Academy. Trabalhou durante 5 anos com gestão de projetos tradicionais e desde 2012 trabalha com métodos ágeis. Participou do comitê estratégico de implantação do SAFe na Softplan, onde atuou durante 6 anos como Scrum Master e RTE (Release Train Engineer). Atualmente, desempenha o papel de Scrum Master e Agile Coach em uma imersão ágil em escala, para projetos de P&D da Intelbras, em parceria com o CITeB (Centro de Inovação e Tecnologia de Biguaçu/SC).",
+            presentation: {
+                title: "Os Desafios e Benefícios da Agilidade para Times Remotos - Dicas e Técnicas para Garantir Resultados Expressivos.",
+                description: "O objetivo desta palestra é apresentar um case de como estamos conseguindo emergir o mindset ágil para 30 pessoas, de 7 times remotos, em um projeto de P&D da Intelbras. Quais os principais desafios enfrentados? Quais as práticas que estão impulsionando as pessoas a serem mais ágeis? Aplicando benefícios do Lean, SAFe, Scrum e Kanban, estamos obtendo resultados expressivos de produtividade e engajamento, auxiliado por métricas de fluxo, projeções probabilísticas, ferramentas de gestão e indicadores de mercado. Atualmente existem tantas boas práticas, métodos, metodologias e frameworks, que a real essência da agilidade acaba sendo esquecida. Seguir uma linha sem considerar o benefício de outra pode se tornar o seu fracasso. Então reflita sobre isso, avalie o seu contexto atual, considere possibilidades, sem julgar que isso ou aquilo é errado. Se esta dando certo? Ok! Porquê não? O verdadeiro crime é não seguir os seus princípios independente da metodologia aplicada.",
+                time: "15:20"
+            }
+        },
+        {
+            name: "Coffee break",
+            time: "16:00"
+        },
+        {
+            name: "Coaracy Silva",
+            photo: "themes/agile-floripa/img/speaker/coracyjunior.jpg",
+            company: "Senior Sistemas",
+            bio: "Minha missão é ajudar a pessoas a se engajar e a atingir resultados. Tenho mais de 20 anos de experiência em gestão empresarial e vivo o mundo de projetos de software a 12 anos. Faço parte da equipe responsável pela transformação ágil na Senior Sistemas.",
+            presentation: {
+                title: "Kanban no Marketing - Como começar?",
+                description: "Você trabalha com marketing? Tem muitos projetos em paralelo? Se perde no meio de tanto trabalho? Não consegue ter visibilidade de tudo que está fazendo? E ainda por cima não consegue mostrar o resultado do seu trabalho? Essa palestra é para você. Começamos em junho/17 a aplicação de método Kanban no time de Marketing (12 pessoas) aqui na Senior Sistemas (Blumenau/SC), do zero mesmo, aplicando os princípios e as práticas. O time não tinha nenhum método estruturado de trabalho e está adorando o método Kanban. Você vai entender que para começar basta dar o primeiro passo!",
+                time: "16:30"
+            }
+        },
+        {
+            name: "Ricardo Delcastanher",
+            photo: "themes/agile-floripa/img/speaker/delcastanher.jpg",
+            company: "SERPRO",
+            bio: "Bacharel em Ciências da Computação, especialista em Software Livre, Certified ScrumMaster e Kanban Management Professional. Empreendedor serial durante mais de uma década, atualmente é Agile Coach no SERPRO em tempo integral. Valoriza pessoas e suas interações mais que processos e ferramentas.",
+            presentation: {
+                title: "Manifesto para Desenvolvimento Ágil de ATIVIDADES NÃO Software ",
+                description: "O Manifesto para Desenvolvimento Ágil de Software precisa ser reescrito? Provavelmente não. É possível ser ágil além do desenvolvimento e o Management 3.0 pode te ajudar nisso. A palestra ambienta o expectador numa discussão do Agile Europe 2016, onde os participantes reclamavam da falta de frameworks ágeis para áreas não software. Passo por uma reflexão sobre “Indivíduos e interações mais que processos e ferramentas” e apresento o Management 3.0 como uma ferramenta para áreas não software, fazendo um paralelo com os princípios do Manifesto Ágil.",
+                time: "17:10"
+            }
+        },
+        {
+            name: "Gabriel Pacheco",
+            photo: "themes/agile-floripa/img/speaker/user.png",
+            company: "Softplan",
+            bio: "Profissional com mais de 10 anos de experiência em TI na área de desenvolvimento de software, em empresas de pequeno e grande porte e também em startup no exterior, atuando ao longo da carreira como Coordenador, Líder, Agile Coach, Scrum Master, Gestor de Projetos, Analista de Requisitos, Desenvolvedor e Consultor",
+            presentation: {
+                title: "Agilidade em Suporte de 2º nível",
+                description: "A ideia é trazer ao público a experiência da aplicação de conceitos ágeis, gestão de pessoas e a Filosofia de Gestão em uma equipe de Suporte de 2º nível com 19 integrantes que nunca ouviram o que é Ágil. Discussão sobre acertos e erros e também apresentar os resultados através dos indicadores de acompanhamento. Como foi possível melhorar o atendimento de SLA, prioridades, e também o clima de trabalho com o uso da agilidade. Mudança de mindset da equipe com foco na entrega de valor no atendimento ao cliente, e um desafio maior, como foi organizar tudo isso com pessoas trabalhando remotamente em 5 cidades diferentes.",
+                time: "17:50"
+            }
+        },
+        {
+            name: "Encerramento + Sorteios e Happy Hour \\o/",
+            time: "18:30"
+        }
     ],
     // List of Sponsors
     sponsors: [
